0e5477d2053457694895dca299295613f7e0ee5d
teastainGit <80008504+teastainGit@users.noreply.github.com>
2021-06-14 22:23:22 -0400

    Arduino sketch for IDE

---
+++ ESPpeer2peer.ino
@@ -0,0 +1,93 @@
+//determine this MAC address by download this sketch first and copying the MAC info
+//from the connect info at the begining of sketch load.
+/*i.e.
+ esptool.py v3.0-dev
+Serial port /dev/cu.usbserial-75522271F6
+Connecting....
+Chip is ESP32-PICO-D4 (revision 1)
+Features: WiFi, BT, Dual Core, 240MHz, Embedded Flash,
+VRef calibration in efuse, Coding Scheme None
+Crystal is 40MHz
+MAC: 94:b9:7e:8c:7c:e8
+ */
+#include <M5StickCPlus.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#define LED 10
+//Adress of OTHER unit        MAC: 94:b9:7e:8c:b9:7c
+uint8_t broadcastAddress[] = {0x94, 0xB9, 0x7E, 0x8C, 0xB9, 0x7c};
+
+String success;
+
+//Structure example to send data
+//Must match the receiver structure  Button.State.Button
+typedef struct struct_message {
+    bool State;
+} struct_message;
+
+// Create a struct_message to hold outgoing button
+struct_message TxButton;
+// Create a struct_message to hold incoming button (to turn on this LED)
+struct_message RxButton;   //I.E. = incomingReadings
+
+void setup() {
+    M5.begin();
+    Serial.begin(115200);
+    pinMode (LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+    WiFi.mode(WIFI_STA);
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(OnDataSent);
+    // Register peer
+    esp_now_peer_info_t peerInfo;
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    // Add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+        Serial.println("A- Failed to add peer");
+        return;
+    }
+    // Register for a callback function that will be called when data is received
+    esp_now_register_recv_cb(OnDataRecv);
+    delay(1000);
+}
+
+void loop() {
+    M5.update();
+    TxButton.State = !M5.BtnA.wasPressed();
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &TxButton, sizeof(TxButton));
+    if (result == ESP_OK) {
+        Serial.println("Sent with success");
+    }
+    else {
+        Serial.println("Error sending the data");
+    }
+    delay(500);
+}
+
+// OnDataRecv when data is received, LED is controlled here
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len)
+{
+    memcpy(&RxButton, incomingData, sizeof(RxButton));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
+    digitalWrite (LED, RxButton.State);
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    Serial.print("\r\nA- Last Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+    if (status == 0) {
+        success = "Delivery Success :)";
+    }
+    else {
+        success = "Delivery Fail :(";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ESP_NOWpeer2peer
- Very simple ezample of using ESP-NOW for peer to peer communications
+ Very simple example of using ESP-NOW for peer to peer communications
+ 
+ I was interested In ESP peer to peer with no client/server, no WiFi router login, not IoT (!!!)
+I ran across a tutorial for "esp_now" and with some difficulty and learning curve I got two M5Stack
+ devices talking peer to peer, perfectly!
+I have created a dozen projects from the simple example below to a JoyC joystick controlling a 
+RoverC and many other applications.
+I made a HVAC controller with wireless reading of duct temperature, transmitted to the Core Basic and
+ in turn the Core transmitting the RC Servo angle for vent damper control...well, this Is how!
+Copied below Is the most simplified esp_now example that I could think of. 
+This example Is ONE sketch for TWO M5StickCs. 
+Only one sketch because the two programs are IDENTICAL except for MAC address
+
